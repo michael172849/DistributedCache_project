d45bc67b8d854e6db6f4d94095b090c0e33b120f
Seth <sethlee@utexas.edu>
2020-05-02 17:22:18 -0500

    add unit test

---
+++ master/hashring_test.py
@@ -0,0 +1,26 @@
+import unittest
+from hashring import MD5HashRing
+
+class TestHashRing(unittest.TestCase):
+    def setUp(self):
+        self.base_servers = ['localhost:{}'.format(i) for i in range(10)]
+        self.ring = MD5HashRing(self.base_servers)
+
+    def test_cache_store(self):
+        cache_urls = ['fb.com/{}'.format(i) for i in range(50)]
+        cache_urls2 = ['bf.com/{}'.format(i) for i in range(50)]
+
+        for url in cache_urls:
+            self.ring.add_cache(url)
+        
+        cache_servers = [self.ring.get_cache_server(url) for url in cache_urls]
+
+        for url in cache_urls2:
+            self.ring.add_cache(url)
+
+        cache_servers_after_adding = [self.ring.get_cache_server(url) for url in cache_urls]
+
+        self.assertEqual(cache_servers, cache_servers_after_adding)
+
+if __name__ == '__main__':
+    unittest.main()
