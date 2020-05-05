550b934a0baf47d6770ab78d8d48f927450f4d89
michael172849 <mich94hj@utexas.edu>
2020-05-04 19:54:07 -0500

    exp script for exp1

--- tests/exp1_driver.py
+++ tests/exp1_driver.py
@@ -2,15 +2,21 @@ import sys
 import os
 import requests
 import logging
+import string
+import random
+
 sys.path.append(os.path.abspath("."))
-from tests import setup
 
 
 logging.basicConfig(level=logging.DEBUG)
 domain = "http://0.0.0.0:5000/"
 content_url = domain + "kv"
+analytics_url = domain + 'analytics'
+
+num_entries = 10
+str_len_a = 3
+str_len_b = 20
 random_data = {}
-exp_steps = [step2]
 
 def sendPostRequests(key, value):
     data = {'key': key, 'value': value}
@@ -24,27 +30,62 @@ def sendGetRequest(key):
     r = requests.get(content_url + '?' + payload)
     logging.info('resp: {0}'.format(r.text))
 
+def stepInc():
+    logging.debug('step increase')
+    r = requests.get(analytics_url + '?' + 'op=step')
+    logging.info('resp: {0}'.format(r.text))
 
-def step2():
+def printAnalytics():
+    logging.debug('print analytics')
+    r = requests.get(analytics_url + '?' + 'op=print')
+    logging.info('resp: {0}'.format(r.text))
+
+def fetch_all():
     for k in random_data:
         sendPostRequests(k)
 
+def generate_random_string(len):
+    pool = string.letters + string.digits + string.punctuation 
+    return ''.join(random.choice(pool) for i in range(len))
+
+def step1():
+    for i in range(num_entries):
+        key = generate_random_string(random.randint(str_len_a, str_len_b))
+        value = generate_random_string(random.randint(str_len_a, str_len_b))
+        random_data[key] = value
+        sendPostRequests(key, value)
+
+def step2():
+    fetch_all()
+
 def step3():
-    pass
+    fetch_all()
 
 def step4():
-    pass
+    fetch_all()
+
+def step5():
+    fetch_all()
+
+
 
 def cmdInterface():
     curStep = 0
+    exp_steps = [step1, step2, step3, step4]
+
     while True:
         cmd = input()
-        if cmd == 'step':
+        if cmd == 's':
             exp_steps[curStep]()
             curStep += 1
+            stepInc()
+        if cmd == 'p':
+            printAnalytics()
         else:
             print('No such command! Available commands are:')
             print('1. s \t \tstep to next step.')
+            print('2. p \t \tprint analytics.')
+
             print()
 
 if __name__ == '__main__':
